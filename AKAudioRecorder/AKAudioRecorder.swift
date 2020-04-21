c14d1d8999bbabeae75626799df1d8f81d07c5a4
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-22 00:10:32 +0530

    AKAudioRecorder Class Created

---
+++ AKAudioRecorder/AKAudioRecorder.swift
@@ -0,0 +1,18 @@
+//
+//  AKAudioRecorder.swift
+//  AKAudioRecorder
+//
+//  Created by Aaryan Kothari on 22/04/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
+
+
+class AKAudioRecorder {
+    
+    static let shared = AKAudioRecorder()
+    
+
+    
+}
