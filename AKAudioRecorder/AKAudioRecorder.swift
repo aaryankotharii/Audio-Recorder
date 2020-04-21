089fdc3cd8bdbe18938754538659f0ead5b7c8cc
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-22 00:21:55 +0530

    Variables added

--- AKAudioRecorder/AKAudioRecorder.swift
+++ AKAudioRecorder/AKAudioRecorder.swift
@@ -7,12 +7,26 @@
 //
 
 import Foundation
+import AVFoundation
 
 
 class AKAudioRecorder {
     
     static let shared = AKAudioRecorder()
     
-
+    //MARK:- Variables
+    private var recordingSession : AVAudioSession = AVAudioSession.sharedInstance()
+    private var audioRecorder : AVAudioRecorder!
+    private var audioPlayer : AVAudioPlayer = AVAudioPlayer()
     
+    private var settings =   [  AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                                AVSampleRateKey: 12000,
+                                AVNumberOfChannelsKey: 1,
+                                AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue ]
+    
+    fileprivate var timer: Timer!
+    
+    private func InitialSetup(){
+        
+    }
 }
