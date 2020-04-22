2971c8d652aab2d55e55c58834594324fb8894ac
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-22 15:00:49 +0530

    exampleVC added

---
+++ ExampleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExampleViewController.swift
+//  AKAudioRecorder
+//
+//  Created by Aaryan Kothari on 22/04/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
