6eb354a57270ad3e3028dadf59a1308185fe73f5
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-22 15:12:54 +0530

    ExampeVC tableView setup done

--- AKAudioRecorder/ExampleViewController.swift
+++ AKAudioRecorder/ExampleViewController.swift
@@ -11,12 +11,34 @@ import UIKit
 class ExampleViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
-    
     @IBOutlet var playButton: UIButton!
     
+    var recorder = AKAudioRecorder.shared
+    
     override func viewDidLoad() {
+        tableView.dataSource = self
+        tableView.delegate = self
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 }
+
+
+extension ExampleViewController: UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recorder.getRecordings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        let recording = recorder.getRecordings[indexPath.row]
+        
+        cell?.detailTextLabel?.text = String(recording)
+        
+        return cell!
+    }
+}
+
+
