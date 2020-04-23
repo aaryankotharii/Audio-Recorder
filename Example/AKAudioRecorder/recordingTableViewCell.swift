75ef732561967e55dbac06c27dcc3073c11d87e0
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-23 13:08:52 +0530

    Final commit

--- Example/AKAudioRecorder/recordingTableViewCell.swift
+++ Example/AKAudioRecorder/recordingTableViewCell.swift
@@ -10,19 +10,22 @@ import UIKit
 
 class recordingTableViewCell: UITableViewCell {
 
+    //MARK:- Cell Outlets
     @IBOutlet var recordingName: UITextField!
     @IBOutlet var fileName: UILabel!
     @IBOutlet var slider : UISlider!
     
+    //MARK:- Variables
     var displayLink = CADisplayLink()
     var recorder = AKAudioRecorder.shared
    
     override func awakeFromNib() {
         super.awakeFromNib()
-        slider.setThumbImage(#imageLiteral(resourceName: "slider"), for: .normal)
-        // Initialization code
+        slider.setThumbImage(#imageLiteral(resourceName: "slider"), for: .normal) ///   SET SLIDER ITHUMB IMAGE
     }
 
+    
+    //MARK:- SET CELL HIGHLIGHT COLOR
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         if selected {
@@ -32,19 +35,24 @@ class recordingTableViewCell: UITableViewCell {
         }
 
     }
+    
+    //MARK:- UPDATE SLIDER
     @objc func updateSliderProgress(){
-         var progress = recorder.getCurrentTime() / Double(recorder.duration)
-         if recorder.isPlaying == false || progress == .infinity{
+        
+         var progress = recorder.getCurrentTime() / Double(recorder.duration) /// progress 0 -> 1
+        
+         if recorder.isPlaying == false || progress == .infinity {
              displayLink.invalidate()
              progress = 0.0
          }
-        slider.value = Float(progress)
+        slider.value = Float(progress)  /// Slider value is equal to progress
      }
      
+    //MARK:- Run Time Loop for slider
      func playSlider(){
-    if recorder.isPlaying{
-     displayLink = CADisplayLink(target: self, selector: #selector(self.updateSliderProgress))
-     self.displayLink.add(to: RunLoop.current, forMode: RunLoop.Mode.default)
+        if recorder.isPlaying{
+            displayLink = CADisplayLink(target: self, selector: #selector(self.updateSliderProgress))
+            self.displayLink.add(to: RunLoop.current, forMode: RunLoop.Mode.default)
          }
      }
 }
